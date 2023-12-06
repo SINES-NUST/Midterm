0468cd95e6675c1979636907b4b0efd5dea0d9a9
alaviabatool <abatool.msbi23sines@student.nust.edu.pk>
2023-12-06 11:09:01 +0500

    all qs done

---
+++ q1.py
@@ -0,0 +1,32 @@
+x = [fiction A, fiction B, non fiction A, fiction C, non fiction B, non fiction C, fiction D, non fiction D]
+# Assigning all books a numerical value
+fiction A = 2
+fiction B = 4
+fiction C = 6
+fiction D = 8
+non fiction A = 3
+non fiction B = 5
+non fiction C = 7
+non fiction D = 9
+# Checking if all have been assigned variables
+print fiction A
+print fiction B
+print fiction C
+print fiction D
+print non fiction A
+print non fiction B
+print non fiction C
+print non fiction D
+print x
+# now we have x=[2,4,3,6,5,7,8,9]
+# Using the modulo operator to sort even from odd numbers
+r equals to x divided by 2 (r=x%2)
+#using if condition 
+if the reminder is 0 
+   print even
+if the reminder is 0
+    print odd
+# Code will print 2 sorted lists, 1 with even numbers and the other with odd numbers
+convert numbers into their strings
+# will get
+[fiction A, fiction B, fiction C, fiction D] [non fiction A, non fiction B, non fiction C, non fiction D]
