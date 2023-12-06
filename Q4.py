6e5b9b92b786e300fe6294f9289081ed767e2a1f
Nimra Chaudhary <nimrajaved926@gmail.com>
2023-12-06 16:04:58 +0500

    Q4 done

---
+++ Q4.py
@@ -0,0 +1,15 @@
+#step1
+import numpy as np
+DNA_Sequence=[['A','G','C','T','T','G','C','G','C']
+              ['G','C','T','A','T','G','C','G','T','T']]
+i=G
+I=C
+while i in DNA_Sequence:
+    print("Content of GC",i)
+    i+=1
+    continue
+print(finished)
+
+#step2
+x=max(len(DNA_Sequence))
+print(x)
