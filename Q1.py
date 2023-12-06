76015af62bda434936a3cd16e654cd0dee61850b
Hirakhan023 <hkhalid.msbi23sines@student.nust.edu.pk>
2023-12-06 12:18:10 +0500

    hira khalid branch created

---
+++ Q1.py
@@ -0,0 +1,15 @@
+Library = ['fiction' , 'non-fiction' books]
+#as the library contains both fiction and non-fiction books]
+#Start
+    create 
+    function library (fiction, non-fiction books)
+    create two lists, one for fiction and one for non-fiction books
+         function  display library();
+         for each fiction book in library:
+             if fiction book is in library:
+                 print ("this is a fiction book")
+                 Else:
+                        print ("this is a non-fiction book")
+                        break from the loop
+            
+            
---
+++ Q4.py
@@ -0,0 +1,27 @@
+initialize #(list of DNA seq contain (A,T,G,C))
+#Calculate GC Content of DNA seq
+#Make a list of DNA seq(GC Content)
+Function DNA seq (G_content, C_content)
+Update the GC content of DNA seq
+return DNA seq
+Function calculate GC content (DNA seq)
+for each GC content in the list of DNA seq 
+     if GC content is equal to the provided GC Content:
+         Add content to the GC Content of DNA seq
+         print "GC content updated successfully"
+#calculate the GC content of DNA seq
+    set total GC content to 0
+    set number of DNA sequences to the length of DNA sequences
+    for each GC i GC content :
+        add GC to total GC content
+        set avrage GC content to total GC content/number of GC content
+        return average GC content
+    #Determine the GC content based on the average score
+    Function Display GC content ();
+        for each GC content in GC content list:
+            if GC content in content list:
+                print (yes,Gc content calculated)
+            else:
+                print (Sorry , GC content not calculated)
+                
+                
