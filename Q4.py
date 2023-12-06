1773d5ddcc6944b0ab89994b3c898295f9b3f346
aleenahamdani <sabbas.msbi23sines@student.nust.edu.pk>
2023-12-06 10:45:45 +0500

    q4

---
+++ Q4.py
@@ -0,0 +1,22 @@
+def DNA_sequence(list):
+    for i==G or j==C in list:
+        GC_content= 100* isinstance(i,int) or isinstance(j,int)/len(list)
+    return GC_content
+def longest_sequence():
+    if L1==max:
+        longest_seq='The longest sequence is L1'
+    elif L2==max:
+        longest_seq='The longest sequence is L2'
+    else:
+        longest_seq='The longest sequence is L3'
+    return longest_sequence
+list=['GTATCTAG']
+L1=['ATGTC']
+L2=['GATATAGTC']
+L3=['CATTTGAAAGATTC']
+
+GC_content=DNA_sequence(list)
+print(GC_content)
+
+longest_seq=longest_sequence()
+print(longest_seq)
