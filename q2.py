96a5dd6ad6c647bb28136b417e964e7d5ac6148d
hina <hgul.msbi23sines@student.nust.edu.pk>
2023-12-11 16:12:00 +0500

    renamed branch to hgul_org

---
+++ q2.py
@@ -0,0 +1,8 @@
+create two lists one for "lower_sales" and one for "average_sales"
+for each employee:
+    assign variables for "lower_sales" and "average_sales" as the lowest and average sales in the lists
+    for each employee's "lower_sales":
+        if the sales are lower than the current "lower_sales" update the "lower_sales" to this mark
+        if the sales are higher than the current "average_sales" update the "average_sales" to this mark
+        display the lowest sale "lower_sales" and the average sale "average_sales"
+        
